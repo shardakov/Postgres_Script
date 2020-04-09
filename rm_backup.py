f97afdbe983ed17780bd1e04454c6ab037d41404
shardakov <dm.shardakov@gmail.com>
2020-04-09 12:29:49 +0300

    add remove backup by name

--- rm_backup.py
+++ rm_backup.py
@@ -7,6 +7,7 @@ Comment
 # python3 rm_backup.py -p */path/*
 WARNING!!! This script save last 7 backups(files)
 in our case is /mnt/backup/*/
+#c:/test
 
 """
 
@@ -15,7 +16,8 @@ folders = []
 parser = argparse.ArgumentParser()
 parser.add_argument('-p', '--path', type=str, help="Input path for dir of backup")
 parse = parser.parse_args()
-backup_dir = parse.path
+#backup_dir = parse.path
+backup_dir = 'c:/test/'
 
 for root, dirs, files in os.walk(backup_dir):
     for folder in dirs:
@@ -23,5 +25,8 @@ for root, dirs, files in os.walk(backup_dir):
 
 for i in folders:
     my_files = [f.name for f in os.scandir(i) if f.is_file()]
-    for file in sorted(my_files, key=lambda p: os.path.getctime(os.path.join(i, p)), reverse=True)[7:]:
-        os.remove(i + "/" + file)
+    for file in sorted(my_files, key=lambda p: os.path.getctime(os.path.join(i, p)), reverse=True)[3:]:
+        if file[:-5] != '01.gz':
+            os.remove(i + "/" + file)
+            print(file + ' was removed')
+        print(file)
--- test.py
+++ test.py
@@ -1,2 +1,30 @@
-s = '1cv82.gz'
-print(s[:-3])
+import os
+import argparse
+
+""" 
+Comment 
+
+# python3 rm_backup.py -p */path/*
+WARNING!!! This script save last 7 backups(files)
+in our case is /mnt/backup/*/
+#c:/test
+
+"""
+
+folders = []
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-p', '--path', type=str, help="Input path for dir of backup")
+parse = parser.parse_args()
+backup_dir = parse.path
+
+for root, dirs, files in os.walk(backup_dir):
+    for folder in dirs:
+        folders.append(os.path.join(root, folder))
+
+for i in folders:
+    my_files = [f.name for f in os.scandir(i) if f.is_file()]
+    for file in sorted(my_files, reverse=True)[3:]:
+        if file[-5:] != '01.gz':
+            os.remove(i + "/" + file)
+            print(file + ' was removed')
